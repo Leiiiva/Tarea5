f56da8a4c24076aaeeb2302c45366f541e6d8e1c
Edgar Leiva <99515843+Leiiiva@users.noreply.github.com>
2022-02-25 20:29:51 -0600

    Completed
    Second cycle added to count the numbers that are multiples of 4 within the range of 0 and the factorial calculated

--- Tarea5/Tarea5/Form1.Designer.vb
+++ Tarea5/Tarea5/Form1.Designer.vb
@@ -35,65 +35,68 @@ Partial Class Form1
         'lbl_01
         '
         Me.lbl_01.AutoSize = True
-        Me.lbl_01.Font = New System.Drawing.Font("Acumin Pro", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.lbl_01.Font = New System.Drawing.Font("Acumin Pro", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
         Me.lbl_01.Location = New System.Drawing.Point(12, 9)
         Me.lbl_01.Name = "lbl_01"
-        Me.lbl_01.Size = New System.Drawing.Size(241, 21)
+        Me.lbl_01.Size = New System.Drawing.Size(279, 25)
         Me.lbl_01.TabIndex = 0
         Me.lbl_01.Text = "Ingrese un numero para iniciar:"
         '
         'lbl_02
         '
         Me.lbl_02.AutoSize = True
-        Me.lbl_02.Font = New System.Drawing.Font("Acumin Pro", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.lbl_02.Location = New System.Drawing.Point(12, 164)
+        Me.lbl_02.Font = New System.Drawing.Font("Acumin Pro", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lbl_02.Location = New System.Drawing.Point(23, 167)
         Me.lbl_02.Name = "lbl_02"
-        Me.lbl_02.Size = New System.Drawing.Size(324, 21)
+        Me.lbl_02.Size = New System.Drawing.Size(310, 21)
         Me.lbl_02.TabIndex = 1
         Me.lbl_02.Text = "Los multiplos de 4 dentro del factorial son:"
         '
         'lbl_03
         '
         Me.lbl_03.AutoSize = True
-        Me.lbl_03.Font = New System.Drawing.Font("Acumin Pro", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.lbl_03.Location = New System.Drawing.Point(37, 99)
+        Me.lbl_03.Font = New System.Drawing.Font("Acumin Pro", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lbl_03.Location = New System.Drawing.Point(37, 91)
         Me.lbl_03.Name = "lbl_03"
-        Me.lbl_03.Size = New System.Drawing.Size(115, 21)
+        Me.lbl_03.Size = New System.Drawing.Size(130, 25)
         Me.lbl_03.TabIndex = 2
         Me.lbl_03.Text = "El factorial es:"
         '
         'lbl_fct
         '
         Me.lbl_fct.AutoSize = True
-        Me.lbl_fct.Font = New System.Drawing.Font("Acumin Pro", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.lbl_fct.Location = New System.Drawing.Point(152, 133)
+        Me.lbl_fct.Font = New System.Drawing.Font("Acumin Pro", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.lbl_fct.ForeColor = System.Drawing.Color.Navy
+        Me.lbl_fct.Location = New System.Drawing.Point(167, 130)
         Me.lbl_fct.Name = "lbl_fct"
-        Me.lbl_fct.Size = New System.Drawing.Size(0, 21)
+        Me.lbl_fct.Size = New System.Drawing.Size(0, 25)
         Me.lbl_fct.TabIndex = 3
         Me.lbl_fct.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'lbl_mult
         '
         Me.lbl_mult.AutoSize = True
-        Me.lbl_mult.Font = New System.Drawing.Font("Acumin Pro", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.lbl_mult.Location = New System.Drawing.Point(12, 202)
+        Me.lbl_mult.Font = New System.Drawing.Font("Acumin Pro", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.lbl_mult.ForeColor = System.Drawing.Color.Navy
+        Me.lbl_mult.Location = New System.Drawing.Point(167, 208)
         Me.lbl_mult.Name = "lbl_mult"
-        Me.lbl_mult.Size = New System.Drawing.Size(0, 21)
+        Me.lbl_mult.Size = New System.Drawing.Size(0, 25)
         Me.lbl_mult.TabIndex = 4
         Me.lbl_mult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'txt_num
         '
         Me.txt_num.Font = New System.Drawing.Font("Acumin Pro", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.txt_num.Location = New System.Drawing.Point(37, 44)
+        Me.txt_num.Location = New System.Drawing.Point(27, 44)
         Me.txt_num.Name = "txt_num"
         Me.txt_num.Size = New System.Drawing.Size(140, 27)
         Me.txt_num.TabIndex = 5
+        Me.txt_num.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
         '
         'btn_calcular
         '
-        Me.btn_calcular.Font = New System.Drawing.Font("Acumin Pro", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.btn_calcular.Location = New System.Drawing.Point(239, 44)
+        Me.btn_calcular.Font = New System.Drawing.Font("Acumin Pro", 12.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point)
+        Me.btn_calcular.Location = New System.Drawing.Point(236, 44)
         Me.btn_calcular.Name = "btn_calcular"
         Me.btn_calcular.Size = New System.Drawing.Size(97, 28)
         Me.btn_calcular.TabIndex = 6
@@ -104,7 +107,7 @@ Partial Class Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(362, 276)
+        Me.ClientSize = New System.Drawing.Size(362, 261)
         Me.Controls.Add(Me.btn_calcular)
         Me.Controls.Add(Me.txt_num)
         Me.Controls.Add(Me.lbl_mult)
--- Tarea5/Tarea5/Form1.vb
+++ Tarea5/Tarea5/Form1.vb
@@ -4,12 +4,19 @@
     End Sub
 
     Private Sub btn_calcular_Click(sender As Object, e As EventArgs) Handles btn_calcular.Click
-        Dim num, x, i As Integer
+        Dim num, x, i, m As Integer
         x = 1
         num = txt_num.Text
         For y = 1 To num
             x = x * y
         Next
         lbl_fct.Text = x.ToString()
+
+        For i = 0 To x
+            If (i Mod 4) Then
+                m = m + 1
+            End If
+        Next
+        lbl_mult.Text = m.ToString()
     End Sub
 End Class
