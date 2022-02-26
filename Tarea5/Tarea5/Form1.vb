d4645a321faef9b602f5b62e6f31541a72a24ccb
Edgar Leiva <99515843+Leiiiva@users.noreply.github.com>
2022-02-25 20:12:12 -0600

    1st For Created
    Created First For to calculate the Fact of the number entered on the text box

--- Tarea5/Tarea5/Form1.vb
+++ Tarea5/Tarea5/Form1.vb
@@ -4,6 +4,12 @@
     End Sub
 
     Private Sub btn_calcular_Click(sender As Object, e As EventArgs) Handles btn_calcular.Click
-
+        Dim num, x, i As Integer
+        x = 1
+        num = txt_num.Text
+        For y = 1 To num
+            x = x * y
+        Next
+        lbl_fct.Text = x.ToString()
     End Sub
 End Class
